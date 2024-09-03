b7322b032af2297e14311fd8e14f9e1aa5e4953c
jvmbrg <jvitormbraga@gmail.com>
2024-09-03 18:18:22 -0300

    Questão 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		/* 1º) 
 		System.out.print("Digite um número: ");
 		int n = sc.nextInt();
 		System.out.println("--------------------------------------------");
@@ -26,5 +27,19 @@ public class Program {
 		}
 		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
-
+	*/
+		System.out.print("Digite uma palavra: ");
+		String palavra = sc.nextLine();
+		char letra = 'a';
+		int total = 0;
+	
+		for(int i=0; i < palavra.length();i++) {
+			if(palavra.toLowerCase().charAt(i) == letra) {
+				total += 1;
+			}
+		}
+		
+		System.out.println("Total de 'a': " + total);
+		sc.close();
+	}
 }
